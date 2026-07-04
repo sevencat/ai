93adcde87c6c205938e4737703a0f9b07a54b4cc
sevencat <bastet.wang@aliyun.com>
2026-07-04 19:35:30 +0800

    net7不打算支持了。TODO:后面改到skiasharp?还是opencvsharp?都太重了啊。

--- sevencat.ai.yolo/Sam3Result.cs
+++ sevencat.ai.yolo/Sam3Result.cs
@@ -14,5 +14,5 @@ public class Sam3Result
 	public int mask_model_width { get; set; }
 	public int mask_model_height { get; set; }
 
-	public List<Sam3ResultItem> items { get; set; } = [];
+	public List<Sam3ResultItem> items { get; set; } = new();
 }
--- sevencat.ai.yolo/util/ImageExt.cs
+++ sevencat.ai.yolo/util/ImageExt.cs
@@ -2,12 +2,61 @@
 using SixLabors.Fonts;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Formats.Png;
 using SixLabors.ImageSharp.Processing;
 
 namespace sevencat.ai.yolo.util;
 
-public static class PlotExt
+public static class ImageExt
 {
+	//JpegEncoder encoder
+	public static string ToBase64JpgUrl(this Image img, JpegEncoder encoder)
+	{
+		var binjpegimg = img.ToJpegImage(encoder);
+		var imgurl = "data:image/jpeg;base64," + Convert.ToBase64String(binjpegimg);
+		return imgurl;
+	}
+
+	public static byte[] ToJpegImage(this Image img, JpegEncoder encoder)
+	{
+		using (var ms = new MemoryStream())
+		{
+			img.SaveAsJpeg(ms, encoder);
+			return ms.ToArray();
+		}
+	}
+
+	public static async Task<string> ToBase64JpgUrl(this Image img)
+	{
+		var binjpegimg = await img.ToJpegImage();
+		var imgurl = "data:image/jpeg;base64," + Convert.ToBase64String(binjpegimg);
+		return imgurl;
+	}
+
+	public static string ToBase64PngUrl(this Image img)
+	{
+		return img.ToBase64String(PngFormat.Instance);
+	}
+
+	public static async Task<byte[]> ToPngImage(this Image img)
+	{
+		using (var ms = new MemoryStream())
+		{
+			await img.SaveAsPngAsync(ms);
+			return ms.ToArray();
+		}
+	}
+
+	public static async Task<byte[]> ToJpegImage(this Image img)
+	{
+		using (var ms = new MemoryStream())
+		{
+			await img.SaveAsJpegAsync(ms);
+			return ms.ToArray();
+		}
+	}
+
 	public static void PlotImage(this Image img, List<DetectionResultItem> bbitems, string fontname = "Arial")
 	{
 		// 预定义一组颜色用于不同类别
